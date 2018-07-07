0bad14bedd9bdeaf047d26b90ddb129f6c5843b0
Sven Luijten <svenluijten1995@gmail.com>
2018-07-07 18:03:58 +0200

    remove unused trait

--- tests/ShortcutsTest.php
+++ tests/ShortcutsTest.php
@@ -2,14 +2,11 @@
 
 namespace Sven\ArtisanShortcuts\Tests;
 
-use GrahamCampbell\TestBenchCore\MockeryTrait;
 use Illuminate\Contracts\Console\Kernel;
 use Sven\ArtisanShortcuts\ShortcutManager;
 
 class ShortcutsTest extends TestCase
 {
-    use MockeryTrait;
-
     /** @test */
     public function it_registers_the_shortcut_as_a_command()
     {
